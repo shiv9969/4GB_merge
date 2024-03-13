1b35d0461a9faaf881eb3dfaa466e13ae188de3d
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-03-13 19:28:28 +0545

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
     TELEGRAM_API = os.environ["TELEGRAM_API"]
     OWNER = os.environ.get("OWNER")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "merge-bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Merge-Bot")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "Sujan_BotZ")
     PASSWORD = os.environ.get("PASSWORD")
     DATABASE_URL = os.environ.get("DATABASE_URL")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
@@ -16,11 +16,10 @@ class Config(object):
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = True
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
-    UPSTREAM_REPO = "https://github.com/Jot43/Videomerge04349/"
+    UPSTREAM_REPO = "https://github.com/SujanCh549/4Gb-VIDEO-MERGE-BOT"
     UPSTREAM_BRANCH = "main"
 
     START_TEXT = """
-Hi Guys, I am Video Merge Bot!
-I can Merge Multiple Videos in One Video. Video Formats should be same.
+Hɪ 👋 I Aᴍ A Fɪʟᴇ/Vɪᴅᴇᴏ Mᴇʀɢᴇ Bᴏᴛ. I Cᴀɴ Mᴇʀɢᴇ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇs!, Aɴᴅ Uᴘʟᴏᴀᴅ Iᴛ Tᴏ Tᴇʟᴇɢʀᴀᴍ.
 
 """
