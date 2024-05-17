f3c10748ebfef0fbe761a8e8931565feb76aa5f3
Sujan Ch <102373673+SujanCh549@users.noreply.github.com>
2024-05-17 17:21:59 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ class MergeBot(Client):
 mergeApp = MergeBot(
     name=Config.SESSION_NAME,
     api_hash=Config.API_HASH,
-    api_id=int(Config.TELEGRAM_API),
+    api_id=Config.TELEGRAM_API,
     bot_token=Config.BOT_TOKEN,
     workers=300,
     plugins=dict(root="plugins"),
