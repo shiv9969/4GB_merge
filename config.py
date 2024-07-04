3bf2a9f6794682e141be28d226e8ad186d400cec
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-07-04 20:29:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Shivji:BoBfiles@cluster0.t1mka5v.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = os.environ.get("LOG_CHANNEL", "-1001826322671")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BAHE2g0AQqL_E73lq4lRIfzVf6SSt5DtHJtdBwtTIJc2ihkuQnEzC4Hq9celhwfr2Xvm_L3CX5P0GnWKebYR100T8vPmqnjIXNOFZ3TPf58Sn2VuDL5r6b8CpVUf4jiQYpPi3WzEzhQON08YZ2Pj-f-PUXxYt-BhNW2SCzy5LrJfMgqGrrhj0p7VYf_YNfgooxiBqaaycf6zjqvrho8hvBcAMqJrCHAlajrenoqHLkLbDWVUXfllXEmvdDLtLupgZVK_yMwBTZgS3lQF3ZfbPmW_Q3o62weI87__kjMXDCTjmq_9Mj3R8JAGdtFQXZvpdWEdw5ncPdJztHi5hPzoga5eNAAXEwAAAAFhkZtXAA")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BAE9PdcARk15elSrxYFC5511RdbqfoABPs48OePG9pbrXLepgewGlV7HKiqN1JB5SakrAx1U9IQch4wNuujjf_X2eVXvStTJm9fh7RBAF4HaPDESQ_k3QYAwMR7gbrvWuwFKIbEwHLSjOVlABTc-T70WPe57MVRUjL2JQPYkuF1eAZAK6lTyS8jBx_NoXXz77pXYCVPF9vkcXnSnxN2sccSGAqFANQKWyYzL8NKsW7N35m-7BQsTRdVH5hWFJjxUVu9vlQbp-qgPaeJL7hGcdCh77InW-ZK4Ymvm8gtYUuIIkhwcwR56F_68jZJdwARn_q0QyrgUP9AYR3BZkuTRkmjmReCMuQAAAAGB6XFoAA")
     IS_PREMIUM = True
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
     UPSTREAM_REPO = "https://github.com/shiv9969/4GB_merge"
